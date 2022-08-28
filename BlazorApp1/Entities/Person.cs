94520f42f125b6d0fb3e0438fe6d07168c93badb
Mahmoud Kassem <modykassem123@gmail.com>
2022-08-29 01:28:36 +0200

    Add project files.

---
+++ BlazorApp1/Entities/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Entities
+{
+    public class Person
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+    }
+}
